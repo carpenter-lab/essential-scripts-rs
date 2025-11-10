0f3f8600b486616e7ef56bc1244fa06efbc2f004
rschauner <rds156@case.edu>
2025-11-10 11:35:23 -0500

    refactor(io): simplify CSV writing logic and remove error boxing
    - Replace `Result` return type with `()`.
    - Simplify error handling by using `expect` for failure cases.
    - Update `LazyFrame` implementation to use `sink_csv` for file writing.

--- src/io.rs
+++ src/io.rs
@@ -1,49 +1,50 @@
 use polars::prelude::*;
-use std::error::Error;
 use std::path::PathBuf;
 
 pub trait WriteToCsvOrStdout {
-    fn write_to_csv_or_stdout(self, output_file: &PathBuf) -> Result<(), Box<dyn Error>>;
+    fn write_to_csv_or_stdout(self, output_file: &PathBuf) -> ();
 }
 
 impl WriteToCsvOrStdout for DataFrame {
-    fn write_to_csv_or_stdout(mut self, output_file: &PathBuf) -> Result<(), Box<dyn Error>> {
+    fn write_to_csv_or_stdout(mut self, output_file: &PathBuf) -> () {
         if *output_file == PathBuf::from("-") {
             let stdout = std::io::stdout();
             let output = stdout.lock();
-            let fin = CsvWriter::new(output).finish(&mut self);
-            match fin {
-                Ok(_) => Ok(()),
-                Err(e) => Err(e.into()),
-            }
+            CsvWriter::new(output)
+                .finish(&mut self)
+                .expect("Failed to write to stdout");
         } else {
             let output = std::fs::File::create(output_file).expect("Failed to create output file");
-            let fin = CsvWriter::new(output).finish(&mut self);
-            match fin {
-                Ok(_) => Ok(()),
-                Err(e) => Err(e.into()),
-            }
+            CsvWriter::new(output)
+                .finish(&mut self)
+                .expect("Failed to write to file");
         }
     }
 }
 
 impl WriteToCsvOrStdout for LazyFrame {
-    fn write_to_csv_or_stdout(self, output_file: &PathBuf) -> Result<(), Box<dyn Error>> {
+    fn write_to_csv_or_stdout(self, output_file: &PathBuf) -> () {
         if *output_file == PathBuf::from("-") {
             let stdout = std::io::stdout();
             let output = stdout.lock();
-            let fin = CsvWriter::new(output).finish(&mut self.clone().collect()?);
-            match fin {
-                Ok(_) => Ok(()),
-                Err(e) => Err(e.into()),
-            }
+            CsvWriter::new(output)
+                .finish(
+                    &mut self
+                        .clone()
+                        .collect()
+                        .expect("Failed to collect lazy frame prior to writing to stdout"),
+                )
+                .expect("Failed to write to stdout");
         } else {
-            let output = std::fs::File::create(output_file).expect("Failed to create output file");
-            let fin = CsvWriter::new(output).finish(&mut self.clone().collect()?);
-            match fin {
-                Ok(_) => Ok(()),
-                Err(e) => Err(e.into()),
-            }
+            self.sink_csv(
+                SinkTarget::Path(PlPath::new(output_file.to_str().unwrap())),
+                CsvWriterOptions::default(),
+                None,
+                SinkOptions::default(),
+            )
+            .expect("Failed to open to CSV file for writing")
+            .collect()
+            .expect("Failed to collect lazy frame prior to writing to file");
         }
     }
 }
